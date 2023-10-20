23a011f6bcf4204c1343d95d33214eaf59fc5f91
Robert Grimwade <79865756+pobertt@users.noreply.github.com>
2023-10-20 16:06:29 +0100

    added stats drop down menu
    hp has been added, rest need work

---
+++ Source/MagicMerchant1/Public/PlayerCharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class MAGICMERCHANT1_API APlayerCharacter : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlayerCharacter();
+	
+	//Player Stats
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = playerStats, meta = (AllowPrivateAccess = "true"))
+	int hp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = playerStats, meta = (AllowPrivateAccess = "true"))
+	int mp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = playerStats, meta = (AllowPrivateAccess = "true"))
+	float money;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = playerStats, meta = (AllowPrivateAccess = "true"))
+	float currency2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = playerStats, meta = (AllowPrivateAccess = "true"))
+	int lvl;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};
+
