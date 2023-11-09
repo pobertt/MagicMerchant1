9f5a5464c8344bb522b830de676db83209f502d9
pobertt <79865756+pobertt@users.noreply.github.com>
2023-11-09 14:00:47 +0000

    creation of item base class

---
+++ Source/MagicMerchant1/Private/Items/ItemBase.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/ItemBase.h"
+
+UItemBase::UItemBase()
+{
+
+}
+
+UItemBase* UItemBase::CreateItemCopy() const
+{
+	UItemBase* ItemCopy = NewObject<UItemBase>(StaticClass());
+
+	ItemCopy->ID = this->ID;
+	ItemCopy->Quantity = this->Quantity;
+	ItemCopy->ItemQuality = this->ItemQuality;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->TextData = this->TextData;
+	ItemCopy->NumericData = this->NumericData;
+	ItemCopy->ItemStatistics = this->ItemStatistics;
+	ItemCopy->AssetData = this->AssetData;
+
+	return ItemCopy;
+}
+
+void UItemBase::SetQuantity(const int32 NewQuantity)
+{
+	if (NewQuantity != Quantity)
+	{
+		//? what to do if true : what to do if false
+		Quantity = FMath::Clamp(NewQuantity, 0, NumericData.bIsStackable ? NumericData.MaxStackSize : 1);
+
+		// deleting item because you cant have 0 items
+		//if (OwningInventory)
+		//{
+		//if(Quantity <= 0)
+		//{
+		//	OwningInventory->RemoveItem(this);
+		//}
+		//}
+	}
+}
+
+void UItemBase::Use(ABetterPlayerCharacter* Character)
+{
+
+}
---
+++ Source/MagicMerchant1/Public/Items/ItemBase.h
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDataStructs.h"
+#include "ItemBase.generated.h"
+#include <BetterPlayerCharacter.h>
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICMERCHANT1_API UItemBase : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	
+	//PROPERTIES AND VARIABLES
+
+	//UPROPERTY()
+	//UInventoryComponent* OwningInventory
+
+	UPROPERTY(VisibleAnywhere, Category = "Item Data", meta = (UIMin=1, UIMax=100))
+	int32 Quantity;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FName ID;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemAssetData AssetData;
+
+	//FUNCTIONS
+
+	UItemBase();
+
+	UItemBase* CreateItemCopy() const;
+
+	//Inlining, copys entire contents of the function to everywhere it will be called. Reduces the overhead of calling
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemStackWeight() const { return Quantity * NumericData.Weight; };
+	
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemSingleWeight() const { return NumericData.Weight; };
+	
+	UFUNCTION(Category = "Item")
+	FORCEINLINE bool IsFullItemStack() const { return Quantity == NumericData.MaxStackSize; };
+	
+	UFUNCTION(Category = "Item")
+	void SetQuantity(const int32 NewQuantity);
+
+	UFUNCTION(Category = "Item")
+	virtual void Use(ABetterPlayerCharacter* Character);
+
+protected:
+	bool operator==(const FName& OtherID) const
+	{
+		return ID == OtherID;
+	}
+};
