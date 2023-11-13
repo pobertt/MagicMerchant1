5f6f23fc553edfdd3c588f3a8507c3d39df24fb4
pobertt <79865756+pobertt@users.noreply.github.com>
2023-11-13 13:07:15 +0000

    added combat tab widget

---
+++ Source/MagicMerchant1/Public/Combat/CombatTabUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombatTabUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICMERCHANT1_API UCombatTabUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
