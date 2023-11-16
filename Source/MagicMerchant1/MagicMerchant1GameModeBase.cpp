7ac8b4cc49b1a5d0ae58f874cab67112a07c1b9d
Robert Grimwade <79865756+pobertt@users.noreply.github.com>
2023-11-16 16:19:30 +0000

    trying to fix multiple combat tab layers and removing it from viewport

--- Source/MagicMerchant1/MagicMerchant1GameModeBase.cpp
+++ Source/MagicMerchant1/MagicMerchant1GameModeBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "MagicMerchant1GameModeBase.h"
+
 #include "UObject/ConstructorHelpers.h"
 #include "Blueprint/UserWidget.h"
 #include "Blueprint/WidgetLayoutLibrary.h"
@@ -18,20 +19,20 @@ void AMagicMerchant1GameModeBase::BeginPlay()
 		CombatWidget = Cast<UCombatTabUserWidget>(CreateWidget(GetWorld(), WidgetClass));
 		
 		//Grabbing reference to main ui blueprint
-		static ConstructorHelpers::FClassFinder<UUserWidget>MainUI(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UserInterface/WBP_UI.WBP_UI'"));
-		
+		//static ConstructorHelpers::FClassFinder<UUserWidget> MainUI(TEXT("/Game/UserInterface/WBP_UI"));
+		// && !MainUI.Succeeded()
 		//Checking whether or not CombatWidget was created successfully 
-		if(CombatWidget != nullptr && !MainUI.Succeeded())
+		if(CombatWidget != nullptr )
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, "UI is Active");
+			return;
 		}
 		else {
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, "This UI is NOT Active");
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, "This UI is not Active");
 			//Grabbing reference to MainUIClass
-			MainUIClass = MainUI.Class;
+			// MainUIClass = MainUI.Class;
 		}
 
-		// UE_LOG(LogTemp, Warning, TEXT("Main UI Class: %s"), *MainUIClass->GetName());
 
 	}
 }
@@ -47,11 +48,12 @@ void AMagicMerchant1GameModeBase::AddCombatWidget()
 void AMagicMerchant1GameModeBase::RemoveCombatWidget()
 {
 	CombatWidget->RemoveFromParent();
+	UWidgetLayoutLibrary::RemoveAllWidgets(GetWorld());
 }
 
 void AMagicMerchant1GameModeBase::AddMainUIWidget()
 {
-	// UUserWidget* UI = CreateWidget<UUserWidget>(this, *MainUIClass);
+	// UUserWidget* UI = CreateWidget<UUserWidget>(GetWorld()->GetGameInstance(), *MainUIClass);
 	// UI->AddToViewport();
 }
 
